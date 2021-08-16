4dc61ff64bce8e513631e2043f24447ea13068e3
Fazil vk <76457800+fazilvk786@users.noreply.github.com>
2021-08-16 17:43:31 +0530

    Create instabot.py

---
+++ instabot.py
@@ -0,0 +1,14 @@
+import instapy
+
+session = instapy(username = "anc.yjoseph",password = "sirajsiru123")
+session.login()
+
+session.set_relationship_bounds(enabled = True, max_followers = 200)
+
+session.set_do_follow(True,percentage=100)
+
+session.like_by_tags(["follow4follow", "messi"], amount = 3)
+
+session.set_dont_like(["sexy","toxic"])
+
+session.end()
